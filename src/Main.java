f28acde179e18dcfd1efe6921deb747134dd79ee
yiquns3 <yiquns3@seas.upenn.edu>
2025-09-24 17:23:53 -0400

    Add conversation: How are you?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("How are you?");
+        System.out.println("I am fine, thank you! And you?");
     }
 }
